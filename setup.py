8f8c9ec82f32d2b6e7daafc4b25f3276e37a27de
Ben Dichter <ben.dichter@gmail.com>
2017-08-22 11:50:35 -0400

    update setup.py to fix install

--- setup.py
+++ setup.py
@@ -53,15 +53,15 @@ setup(
     ],
 
     # What does your project relate to?
-    keywords='data visualization',
+    keywords='face recognition',
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages=find_packages(exclude=['docs']),
+    #packages=find_packages(exclude=['docs']),
 
     # Alternatively, if you want to distribute just a my_module.py, uncomment
     # this:
-    #   py_modules=["my_module"],
+    py_modules=["face_extraction"],
 
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
